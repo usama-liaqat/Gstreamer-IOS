77176b2e2e5a329230a75eddd3ab9431c5978c07
Usama Liaqat <usama@ocyber.io>
2023-04-17 07:16:15 +0500

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Usama Liaqat on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
